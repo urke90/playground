57976a396b916d9465e9b3467b2a6eec84fce11a
urke90 <urosbijelic90@gmail.com>
2023-05-14 15:54:09 +0200

    mui init setup deps, store, provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { useTheme } from '@mui/material';
 import Router from './routes';
 
 const App: React.FC = () => {
+    const theme = useTheme();
+    console.log('theme', theme);
     return (
         <div>
             <Router />
